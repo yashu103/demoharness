338e1e3702182f0626856576f2f13c6f2e5cf5d3
yashu103 <102040891+yashu103@users.noreply.github.com>
2025-01-29 12:24:07 +0530

    Update cypress.config.js

--- my-cypress-project/cypress.config.js
+++ my-cypress-project/cypress.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    e2e: {
-      baseUrl: 'https://example.cypress.io',
-      setupNodeEvents(on, config) {
-        // Node event listeners here if needed
-      },
+  e2e: {
+    baseUrl: 'https://example.cypress.io',
+    supportFile: false, // Disable support file if not needed
+    setupNodeEvents(on, config) {
+      // Add event listeners here if needed
     },
-  };
+  },
+};
